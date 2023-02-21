222b84aa8151132ed9505bd2c5339d392243dd8c
hiroalchem <h.kawai888@gmail.com>
2023-02-21 13:54:05 +0900

    first commit

---
+++ src/napari_large_image_importer/_widget.py
@@ -0,0 +1,30 @@
+"""
+This module is an example of a barebones QWidget plugin for napari
+
+It implements the Widget specification.
+see: https://napari.org/stable/plugins/guides.html?#widgets
+
+Replace code below according to your needs.
+"""
+from typing import TYPE_CHECKING
+
+from magicgui import magic_factory
+from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
+
+if TYPE_CHECKING:
+    import napari
+
+
+class NliiQWidget(QWidget):
+    def __init__(self, napari_viewer):
+        super().__init__()
+        self.viewer = napari_viewer
+
+        btn = QPushButton("Click me!")
+        btn.clicked.connect(self._on_click)
+
+        self.setLayout(QHBoxLayout())
+        self.layout().addWidget(btn)
+
+    def _on_click(self):
+        print("napari has", len(self.viewer.layers), "layers")
